bf7568ba5d5bb3324ff7e3554a1c30ee21bbc838
Mathéo Deleplanque <matheo.deleplanque@gmail.com>
2025-12-28 21:20:55 +0100

    fix(coming meetings): Fix a url format problem + the selection of meetings to display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,36 @@ import { useEffect, useState } from "react";
 import { Meeting } from "@/types";
 import MeetingReduceCard from "@/components/UI/Cards/MeetingReduceCard";
 import { useSession } from "next-auth/react";
+import {getFormattedDate} from "@/utils/utils";
 
 // ==============================================
 
 const HomePage = () => {
     const session = useSession();
-    const today: string = new Date().toLocaleDateString().split("T")[0];
-
     const [meetings, setMeetings] = useState<Meeting[]>([]);
 
     useEffect(() => {
         const func = async () => {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/meetings?startHour=${today}`);
+            const today: Date = new Date();
+            const formattedTodayDate: string = getFormattedDate(today);
+            const daysPrevisualisationNumber: number = 3;
+
+            const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/meetings?startHour=${formattedTodayDate}`);
             if(response.ok){
                 let datas = await response.json();
-                datas = datas.slice(0, 3);
-                setMeetings(datas);
+
+                const nextThreeDays: string[] = Array.from({ length: daysPrevisualisationNumber }, (_, index) => {
+                    const todayDate: Date = new Date();
+                    todayDate.setDate(todayDate.getDate() + index);
+                    return getFormattedDate(todayDate);
+                });
+
+                const sortedMeetings: Meeting[] = nextThreeDays.map((dateStr: string) => {
+                    const meetingFound: Meeting = datas.find((meeting: Meeting) => meeting.startHour.startsWith(dateStr));
+                    return meetingFound || null;
+                });
+
+                setMeetings(sortedMeetings);
             }
         };
         func();
@@ -34,7 +48,13 @@ const HomePage = () => {
           <h1>Bonjour {session.data?.user?.name} !</h1>
           <div className={styles.homePageSectionRow}>
               <div style={{width: "50%"}} className={styles.comingSoonMeetingsDiv}>
-                  {meetings.map((meeting: Meeting | null, index: number) => <MeetingReduceCard key={index} weekDay={meeting?.startHour ?? ""} meetingTitle={meeting?.title} />)}
+                  {meetings.map((meeting: Meeting | null, index: number) => {
+                      const todayDate = new Date();
+                      todayDate.setDate(todayDate.getDate() + index);
+                      const dateLabel: string = getFormattedDate(todayDate);
+
+                      return <MeetingReduceCard key={index} weekDay={meeting ? meeting.startHour : dateLabel} meetingTitle={meeting?.title}/>
+                  })}
               </div>
               <div className={styles.kpisDiv}>
                   <KpiCard name="Clients" value={12} />
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -66,3 +66,11 @@ export const getWeekDay = (day: number): string => {
             return day.toString()
     }
 };
+
+export const getFormattedDate = (date: Date): string => {
+    const year: number = date.getFullYear();
+    const month: string = String(date.getMonth() + 1).padStart(2, '0');
+    const day: string = String(date.getDate()).padStart(2, '0');
+
+    return `${year}-${month}-${day}`;
+};
