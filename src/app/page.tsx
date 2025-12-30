f05cea22c0a23872c95b3e8be2d2a71ee94d8d0a
Mathéo Deleplanque <matheo.deleplanque@gmail.com>
2025-12-30 22:26:05 +0100

    feat(home page): Adding a V1 of lazy loading interface (without loading.ts) via skeleton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,96 +3,72 @@ import HomeSideBar from "@/components/Layout/HomeSideBar";
 import styles from "./homepage.module.css";
 import KpiCard from "@/components/UI/Cards/KpiCard";
 import { useEffect, useState } from "react";
-import { Meeting, File} from "@/types";
+import {Meeting, File, Client, Project} from "@/types";
 import MeetingReduceCard from "@/components/UI/Cards/MeetingReduceCard";
 import { useSession } from "next-auth/react";
 import { getFormattedDate } from "@/utils/utils";
 import toast from "react-hot-toast";
 import FileCard from "@/components/UI/Cards/FileCard";
+import Skeleton from "@/components/UI/Skeleton";
 
 // ==============================================
 
 const HomePage = () => {
     const session = useSession();
     const today: Date = new Date();
-    const formattedTodayDate: string = getFormattedDate(today);const [meetings, setMeetings] = useState<any[]>([]);
+    const formattedTodayDate: string = getFormattedDate(today);
 
-    const [clientsNumber, setClientsNumber] = useState<number>(0);
-    const [processingProjectNumber, setProcessingProjectNumber] = useState<number>(0);
+    const [meetings, setMeetings] = useState<any[]>([]);
+    const [clients, setClients] = useState<Client[]>([]);
+    const [processingProjects, setProcessingProjects] = useState<Project[]>([]);
     const [recentFiles, setRecentFiles] = useState<File[]>([]);
+    const [dataAreLoading, setDataAreLoading] = useState<boolean>(false);
 
     useEffect(() => {
         const func = async () => {
-            const clientsResponse = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/clients`);
-            if(clientsResponse.ok){
-                const clients = await clientsResponse.json();
-                setClientsNumber(clients.length);
-            }
-            else{
-                const errorMessage = await clientsResponse.json();
-                toast.error(errorMessage.error);
-            }
+            setDataAreLoading(true);
+            const responses = await Promise.all([
+                fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/clients`),
+                fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/projects?endDate=${formattedTodayDate}`),
+                fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/files`),
+                fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/meetings?startHour=${formattedTodayDate}`)
+            ]);
 
-            const projectsResponse = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/projects?endDate=${formattedTodayDate}`);
-            if(projectsResponse.ok){
-                const projects = await projectsResponse.json();
-                setProcessingProjectNumber(projects.length);
-            }
-            else{
-                const errorMessage = await projectsResponse.json();
-                toast.error(errorMessage.error);
-            }
+            const data = await Promise.all(
+                responses.map(async (response) => {
+                    const datas = await response.json();
+                    if (!response.ok){
+                        toast.error(datas.error);
+                        return [];
+                    }
+                    return datas;
+                })
+            );
 
-            if(clientsResponse.ok && projectsResponse.ok){
-                toast.success("Récupération des KPIs terminée");
-            }
-        };
-        func();
-    }, []);
+            const [clientsData, projectsData, recentFilesData, meetingsData] = data;
 
-    useEffect(() => {
-        const func = async () => {
+            setClients(clientsData);
+            setProcessingProjects(projectsData);
+            setRecentFiles(recentFilesData);
+
             const daysPrevisualisationNumber: number = 3;
-            const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/meetings?startHour=${formattedTodayDate}`);
-            let datas = await response.json();
 
-            if(response.ok){
-                const nextThreeDays: string[] = Array.from({ length: daysPrevisualisationNumber }, (_, index) => {
-                    const todayDate: Date = new Date();
-                    todayDate.setDate(todayDate.getDate() + index);
-                    return getFormattedDate(todayDate);
-                });
+            const nextThreeDays: string[] = Array.from({ length: daysPrevisualisationNumber }, (_, index) => {
+                const todayDate: Date = new Date();
+                todayDate.setDate(todayDate.getDate() + index);
+                return getFormattedDate(todayDate);
+            });
 
-                const sortedMeetings: Meeting[] = nextThreeDays.map((dateStr: string) => {
-                    const meetingFound: Meeting = datas.find((meeting: Meeting) => meeting.startHour.startsWith(dateStr));
-                    return meetingFound || null;
-                });
+            const sortedMeetings: Meeting[] = nextThreeDays.map((dateStr: string) => {
+                const meetingFound: Meeting = meetingsData.find((meeting: Meeting) => meeting.startHour.startsWith(dateStr));
+                return meetingFound || null;
+            });
 
-                setMeetings(sortedMeetings);
-                toast.success("Réunion proches récupérées");
-            }
-            else{
-                setMeetings([null, null, null]);
-                toast.error(datas.error);
-            }
-        };
-        func();
-    }, []);
+            setMeetings(sortedMeetings);
 
-    useEffect(() => {
-        const func = async () => {
-            const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/files`);
-
-            if(response.ok){
-                let datas = await response.json();
-                setRecentFiles(datas);
-                toast.success("Fichiers récents récupérés");
-            }
-            else{
-                const errorMessage = await response.json();
-                toast.error(errorMessage.error);
-            }
-        };
+            setDataAreLoading(false);
+            toast.success("Données récupérés avec succès");
+        }
         func();
     }, []);
 
@@ -103,7 +79,14 @@ const HomePage = () => {
           <h1>Bonjour {session.data?.user?.name} !</h1>
           <div className={styles.homePageSectionRow}>
               <div style={{width: "50%"}} className={styles.comingSoonMeetingsDiv}>
-                  {meetings.map((meeting: Meeting | null, index: number) => {
+                  {
+                      dataAreLoading ? Array.from({length: 3}).map((_, index) => (
+                          <div key={index} style={{width: "100%"}}>
+                              <Skeleton width="100%" height="30px" />
+                              <br/>
+                              <Skeleton width="100%" height="100px" />
+                          </div>
+                      )) : meetings.map((meeting: Meeting | null, index: number) => {
                       const todayDate = new Date();
                       todayDate.setDate(todayDate.getDate() + index);
                       const dateLabel: string = getFormattedDate(todayDate);
@@ -112,15 +95,19 @@ const HomePage = () => {
                   })}
               </div>
               <div className={styles.kpisDiv}>
-                  <KpiCard name="Clients" value={clientsNumber} />
-                  <KpiCard name="Projets en cours" value={processingProjectNumber} />
+                  {dataAreLoading ? <Skeleton width="100%" height="100%" /> : <KpiCard name="Clients" value={clients.length} />}
+                  {dataAreLoading ? <Skeleton width="100%" height="100%" /> : <KpiCard name="Projets en cours" value={processingProjects.length} />}
               </div>
           </div>
           <div className={styles.homePageSectionRow}>
               <div style={{width: "50%"}} className={styles.recentFilesDiv}>
                   <label>Fichiers récents:</label>
                   <div className={styles.filesDiv}>
-                      {recentFiles.map((file: File) => <FileCard key={file.id} file={file} />)}
+                      {
+                          dataAreLoading ? Array.from({length: 4}).map((_, index) => (
+                              <Skeleton key={index} width="100%" height="200px" />
+                              )) : recentFiles.map((file: File) => <FileCard key={file.id} file={file} />)
+                      }
                   </div>
               </div>
               <div style={{width: "50%"}}></div>
